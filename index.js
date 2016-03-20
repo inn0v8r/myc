9f71a13ae9092c835dd3be1ff8b0be323b5ea82c
Konrad lagarde <Lagarde@us.ibm.com>
2016-03-19 22:28:51 -0400

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+app.post('/foo', function(req, res) {
+
+  var request = require('request');
+  console.log("in post");
+  var options={'text':req.body.Subject+" was send from "+req.body.From
+              }
+ //console.log(req.body);
+  request({
+    url: "<mywebhook>",
+    method: "POST",
+    json: true,   // <--Very important!!!
+    body: options
+}, function (error, response, body){
+    console.log(response);
+});
+res.end("");
+
+});
